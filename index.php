29f8fd9f8c6712ba1223319f4359f5227aa2d17a
Shadocl-low <vt231_poi@student.ztu.edu.ua>
2025-06-16 17:59:54 +0300

    Created new files

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Генератор невероятных приключений</title>
+    <script src="www/js/move_to_php.js"></script>
+</head>
+<body>
+
+<p>NEW</p>
+
+</body>
+</html>
